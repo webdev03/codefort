e48712f70321b255fce78edc4859dd561aa04966
Devarsh <75148774+webdev03@users.noreply.github.com>
2025-04-07 21:28:47 +1200

    typescript bun

---
+++ languages/typescript-bun/run.bash
@@ -0,0 +1,4 @@
+# Bun needs to be installed system-wide!
+# Or alternatively add the /home/<user>/.bun directory to `neededDirs` in meta.json, and change this script to:
+# /home/<user>/.bun/bin/bun run code.ts
+bun run code.ts
